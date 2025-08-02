cd9f527102b652742cea3723d52beab0a3aa5d57
Jeiner <jeinerarenas435@gmail.com>
2025-08-02 16:09:21 -0500

    guardo cambios  principales

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/components/home/home';
+import { AboutUs } from './pages/components/about-us/about-us';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home},
+  { path: 'about-us', component: AboutUs},
+
+];
