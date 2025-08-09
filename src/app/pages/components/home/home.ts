9102d923828393cb1c3e9f875d1cffc81ae26e37
Jeiner <jeinerarenas435@gmail.com>
2025-08-09 14:49:23 -0500

    Cambios menores

--- src/app/pages/components/home/home.ts
+++ src/app/pages/components/home/home.ts
@@ -1,13 +1,20 @@
 import { Component, signal } from '@angular/core';
 import { Header } from '../../../components/header/header';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCheckCircle, faCogs, faCubes, faHandshake, faStar, faUniversity, faUser } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home',
-  imports: [Header],
+  imports: [Header, FontAwesomeModule],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
 export default class Home {
-
+  faUser = faUser;
+  faCheckCircle = faCheckCircle;
+  faCubes = faCubes;
+  faHandshake = faHandshake;
+  faStar = faStar;
+  faUniversity = faUniversity;
+  faCogs = faCogs;
 }
