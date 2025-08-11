7e8853ffea88dfc98b1c44ca3f81d0c3002e4a8a
Jeiner <jeinerarenas435@gmail.com>
2025-08-11 07:42:48 -0500

    guarda cambios

--- src/app/pages/components/home/home.ts
+++ src/app/pages/components/home/home.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { Header } from '../../../components/header/header';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faCheckCircle, faCogs, faCubes, faHandshake, faStar, faUniversity, faUser } from '@fortawesome/free-solid-svg-icons';
+import { HomeContact } from "../home-contact/home-contact";
 
 @Component({
   selector: 'app-home',
-  imports: [Header, FontAwesomeModule],
+  imports: [Header, FontAwesomeModule, HomeContact],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
