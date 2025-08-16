fb021558c663606a12dcc3bd16c5a41a44bb92d5
Jeiner <jeinerarenas435@gmail.com>
2025-08-16 18:51:25 -0500

    cambios

--- src/app/pages/components/contact/contact.ts
+++ src/app/pages/components/contact/contact.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { Header } from "../../../components/header/header";
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [Header],
   templateUrl: './contact.html',
   styleUrl: './contact.css'
 })
