83d94b18b5e8bf140183ca1a0e19e9f2a69791b8
Eider Prieto <prietoeider1@gmail.com>
2025-08-16 09:31:43 -0500

    configuracion de firebase

--- src/app/services/database.service.ts
+++ src/app/services/database.service.ts
@@ -1,8 +1,17 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { Database, get, ref } from '@angular/fire/database';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DatabaseService {
-  
+
+
+  private db = inject(Database);
+
+  async getCollection(collectionName: string) {
+    const collection = ref(this.db, collectionName);
+    const snapshot = await get(collection);
+    return snapshot.exists() ? snapshot.val() : null;
+  }
 }
