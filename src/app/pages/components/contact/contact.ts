f7cd4a682d728e58d77cd0b77152ae40086f6182
Eider Prieto <prietoeider1@gmail.com>
2025-08-17 17:53:01 -0500

    update

--- src/app/pages/components/contact/contact.ts
+++ src/app/pages/components/contact/contact.ts
@@ -1,16 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Header } from "../../../components/header/header";
 import { Footer } from '../../../components/footer/footer';
+import { CommonModule } from '@angular/common';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-contact',
   imports: [
+    CommonModule,
     Header,
     Footer,
+    TranslateModule,
+    RouterLink,
+    RouterLinkActive,
   ],
   templateUrl: './contact.html',
   styleUrl: './contact.css'
 })
 export class Contact {
 
+  readonly router = inject(Router);
+  private readonly translate = inject(TranslateService);
 }
