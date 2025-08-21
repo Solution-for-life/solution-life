5ae60521eb05091e5c2859f0cee6e99b37d797b7
Eider Prieto <prietoeider1@gmail.com>
2025-08-21 09:13:27 -0500

    configuracion del idioma del backend

--- src/app/interfaces/service.ts
+++ src/app/interfaces/service.ts
@@ -1,9 +1,13 @@
 export interface Service {
     id?: string;
-    title: string;
-    briefDescription: string;
-    description: string;
+    title: language ;
+    briefDescription: language ;
+    description: language ;
     icon?: string;
     createdAt?: Date;
     updatedAt?: Date;
+}
+interface language {
+    en: string;
+    es: string;
 }
