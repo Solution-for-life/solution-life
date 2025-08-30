6f1dff0b64dccd3ba3a950aebe671e57fae07373
Jeiner <jeinerarenas435@gmail.com>
2025-08-30 10:25:58 -0500

    guardar cambios

--- src/app/pages/components/about-us/about-us.ts
+++ src/app/pages/components/about-us/about-us.ts
@@ -2,13 +2,15 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Header } from '../../../components/header/header';
 import { RouterLink } from '@angular/router';
+import { Footer } from "../../../components/footer/footer";
 
 @Component({
   selector: 'app-about-us',
   imports: [
     CommonModule,
     Header,
-    RouterLink
+    RouterLink,
+    Footer
 ],
   templateUrl: './about-us.html',
   styleUrl: './about-us.css'
