481b42666840aef66e6a955da0b8e5c4a0c58f9e
Jeiner <jeinerarenas435@gmail.com>
2025-09-05 13:06:43 -0500

    guardar cambios

--- src/app/pages/components/home-contact/home-contact.ts
+++ src/app/pages/components/home-contact/home-contact.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CarruselComponent } from "../carrusel/carrusel";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home-contact',
-  imports: [CarruselComponent],
+  imports: [CarruselComponent, RouterLink],
   templateUrl: './home-contact.html',
   styleUrl: './home-contact.css'
 })
