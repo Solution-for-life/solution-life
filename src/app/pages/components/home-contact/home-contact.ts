62f9bf9969f4db4eacbfb774277e241ca3b4d12c
Jeiner <jeinerarenas435@gmail.com>
2025-09-06 14:25:52 -0500

    guardar cambios

--- src/app/pages/components/home-contact/home-contact.ts
+++ src/app/pages/components/home-contact/home-contact.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CarruselComponent } from "../carrusel/carrusel";
 import { RouterLink } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-home-contact',
-  imports: [CarruselComponent, RouterLink],
+  imports: [CarruselComponent, RouterLink, TranslateModule],
   templateUrl: './home-contact.html',
   styleUrl: './home-contact.css'
 })
